25e1d6142e98a00260851e402e9aebaf392f8aac
Sia <siavash_p@hotmail.com>
2025-03-14 20:57:26 -0700

    Added svg logo, put together a menu, and few components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavigationComponent } from "./components/main-navigation/main-navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/main-navigation/main-navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main-navigation',
+  imports: [RouterModule],
+  templateUrl: './main-navigation.component.html',
+  styleUrl: './main-navigation.component.scss'
+})
+export class MainNavigationComponent {
+
+}
