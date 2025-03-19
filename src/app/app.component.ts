c860dfbbe40a1bb1dd01229bed401c0c24500a29
Sia <siavash_p@hotmail.com>
2025-03-19 15:16:55 -0700

    Added a footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainNavigationComponent } from "./components/main-navigation/main-navigation.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainNavigationComponent],
+  imports: [RouterOutlet, MainNavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
