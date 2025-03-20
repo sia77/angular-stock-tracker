f609ac8fbb82f04586156ac4c3d13a7ba32fdd96
Sia <siavash_p@hotmail.com>
2025-03-19 20:20:47 -0700

    Installed Angular Material

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
