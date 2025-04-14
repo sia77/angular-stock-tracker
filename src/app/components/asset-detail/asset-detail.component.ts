f71d0a76014032c6f3e7daccfacae2a612e787ee
Sia <siavash_p@hotmail.com>
2025-04-14 13:27:21 -0700

    Created a new component for chart, and fixed some of responsive issues with the chart and drop-down menu

--- src/app/components/asset-detail/asset-detail.component.ts
+++ src/app/components/asset-detail/asset-detail.component.ts
@@ -10,14 +10,12 @@ import { LargeNumberFormatPipe } from "../../pipes/large-number-format.pipe";
 import { AssetNewsService } from '../../services/asset-news.service';
 import { NewsItem } from '../../interface/news';
 import { ShortExchangeNamePipe } from "../../pipes/short-exchange-name.pipe";
-import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-
+import { ChartComponent } from "../chart/chart.component";
 
 
 @Component({
   selector: 'app-asset-detail',
-  imports: [CommonModule, MatDialogModule, MatIconModule, LargeNumberFormatPipe, ShortExchangeNamePipe, BaseChartDirective],
+  imports: [CommonModule, MatDialogModule, MatIconModule, LargeNumberFormatPipe, ShortExchangeNamePipe, ChartComponent],
   templateUrl: './asset-detail.component.html',
   styleUrl: './asset-detail.component.css'
 })
@@ -39,29 +37,24 @@ export class AssetDetailComponent implements OnInit {
   private assetNews!:Subscription;
   private additonalMetrics!:Subscription;
 
-  chartData!: ChartData<'line'>;
-  chartOptions!: ChartOptions;
-  chartLegend:boolean = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public dialogBoxData: any,
     private assetDetailsService: AssetDetailsService,
     private assetNewsService: AssetNewsService,
     private dialogRef: MatDialogRef<AssetDetailComponent> // Needed for closing dialog
-  ) {
-          // Register Chart.js components
-          Chart.register(...registerables);
-  }
+  ) { }
 
   
   ngOnInit(): void {
 
     const today = new Date();
-    //const yesterday = new Date();
-    //yesterday.setDate(yesterday.getDate()-1);
-    const formattedDate = today.toLocaleDateString('en-CA');
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate()-1);
     
-    //const formattedYesterday = yesterday.toISOString().split('T')[0];
+    //Due to the type of subscription, recent SIP data is not available so we alway look at yester as the ending date
+    const formattedDateYesterday = yesterday.toLocaleDateString('en-CA'); 
+    const fomattedDateToday = today.toLocaleDateString('en-CA');
     
     const _5years = new Date();
     _5years.setFullYear(_5years.getFullYear() - 5);
@@ -82,15 +75,12 @@ export class AssetDetailComponent implements OnInit {
       });
 
     this.dailyHistoricalBars = this.assetDetailsService
-      .getDailyHistoricalBar(this.dialogBoxData.ticker.toUpperCase(), formattedDate5Year, formattedDate)
+      .getDailyHistoricalBar(this.dialogBoxData.ticker.toUpperCase(), formattedDate5Year, formattedDateYesterday)
       .subscribe({
         next: (data)=>{
-          console.log("hello: ", data);
           this.alpaca_historical_bar = data.bars;
-          this. drawChart( this.dialogBoxData.ticker.toUpperCase(), this.alpaca_historical_bar /*formattedDate5Year, formattedDate*/);
         }
       });
-
 
     this.additonalMetrics = this.assetDetailsService
       .getAdditionalAssetMetrics(this.dialogBoxData.ticker.toUpperCase())
@@ -100,15 +90,18 @@ export class AssetDetailComponent implements OnInit {
         }
       });
 
+    this.getAssetNews(fomattedDateToday);
+  } 
+  
+  getAssetNews(today:string){
     this.assetNews = this.assetNewsService
-      .getNewsService(this.dialogBoxData.ticker.toUpperCase(), formattedDate, formattedDate)
+      .getNewsService(this.dialogBoxData.ticker.toUpperCase(), today, today)
       .subscribe({
         next: (data)=>{
           this.newsList = data;          
         }
       });
-
-  }  
+  }
 
   closeDialog() {
     this.dialogRef.close();
@@ -118,55 +111,12 @@ export class AssetDetailComponent implements OnInit {
     return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
   }
 
-  drawChart(ticker:string, barData:BarData[]){
-
-    if(!barData) return;
-    
-    const lineCharLabels = barData.map(item => item.t.split('T')[0]);
-    const lineChartData = barData.map(item => item.c);
-    const volumeData = barData.map(item => item.v);
-
-    this.chartData = {
-      labels: lineCharLabels,
-      datasets: [
-        {
-          label: 'Closing',
-          data: lineChartData,
-          borderColor: '#1382aa',
-          backgroundColor: 'rgba(0, 0, 255, 0.1)',
-          fill: false,
-        },
-        
-      ],
-    };
-    
-    this.chartOptions = {
-      responsive: true,
-      scales: {
-        x: {
-          title: {
-            display: false,
-            text: 'Year',
-          },
-        },
-        y: {
-          title: {
-            display: false,
-            text: 'Price',
-          },
-        },
-      },
-    };    
-      
-  }
-
   ngOnDestroy(): void {
     this.assetDetailSub?.unsubscribe();
     this.assetBarSub?.unsubscribe();
     this.dailyHistoricalBars?.unsubscribe();
     this.assetNews?.unsubscribe();
     this.additonalMetrics?.unsubscribe();
-    //myLineChart.destroy();
   }
 }
 
---
+++ src/app/components/chart/chart.component.ts
@@ -0,0 +1,97 @@
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, input, OnDestroy, ViewChild } from '@angular/core';
+import { Chart, ChartConfiguration, ChartData, ChartOptions, ChartType, registerables } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { BarData } from '../../interface/assetInterfaces';
+
+@Component({
+  selector: 'app-chart',
+  imports: [BaseChartDirective],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.css'
+})
+export class ChartComponent implements AfterViewInit, OnDestroy {
+
+  constructor(private cdr: ChangeDetectorRef){
+    Chart.register(...registerables);
+  }
+
+  readonly barsData = input<BarData[]>();
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+  @ViewChild('chartWrapper', { static: true }) chartWrapperRef!: ElementRef;
+
+  public chartType: ChartType = 'line';
+  chartData!: ChartData<'line'>;
+  public chartOptions: ChartConfiguration['options'] = {};
+
+  private resizeObserver?: ResizeObserver;
+  private lastIsPortrait: boolean | null = null;
+  private resizeTimeout: any;
+
+  ngAfterViewInit(): void {
+    this.applyResponsiveOptions();
+
+    this.resizeObserver = new ResizeObserver(() => {
+      clearTimeout(this.resizeTimeout);
+      this.resizeTimeout = setTimeout(() => {
+        this.applyResponsiveOptions();
+        this.drawChart(this.barsData())
+      }, 200); // debounce
+    });
+
+    this.resizeObserver.observe(this.chartWrapperRef.nativeElement);
+  }
+
+  drawChart(barData:BarData[] | undefined){
+
+    if(!barData) return;    
+    
+    const lineCharLabels = barData.map(item => item.t.split('T')[0]);
+    const lineChartData = barData.map(item => item.c);
+    const volumeData = barData.map(item => item.v);
+
+    this.chartData = {
+      labels: lineCharLabels,
+      datasets: [
+        {
+          label: 'Closing',
+          data: lineChartData,
+          borderColor: '#1382aa',
+          backgroundColor: 'rgba(0, 0, 255, 0.1)',
+          fill: false,
+        },        
+      ],
+    };  
+    this.cdr.detectChanges();
+    this.chart?.update();    
+  }
+
+  ngOnDestroy(): void {
+    this.resizeObserver?.disconnect();
+    clearTimeout(this.resizeTimeout);
+  }
+
+  private applyResponsiveOptions(): void {
+    const isPortrait = window.innerHeight > window.innerWidth;
+
+    // Only update if orientation has changed
+    if (this.lastIsPortrait === isPortrait) return;
+
+    this.lastIsPortrait = isPortrait;
+
+    this.chartOptions = {
+      responsive: true,
+      maintainAspectRatio: true,
+      aspectRatio: isPortrait ? 1 : 2, // 1:1 for portrait, 2:1 for landscape
+      plugins: {
+        legend: { display: true },
+      },
+    };
+
+    setTimeout(() => {
+      console.log("setTimeout");
+      this.chart?.chart?.destroy();
+      this.chart?.render();
+    });
+  }
+}
